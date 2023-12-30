e93b67425d269b1c545fc5abecfbb15c605dcd4c
Awdhesh9211 <gaundawdhesh9211@gmail.com>
2023-12-30 10:02:37 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function toggleMenu() {
+    var navbar = document.querySelector('.navbar');
+    navbar.style.display = (navbar.style.display === 'none' || navbar.style.display === '') ? 'flex' : 'none';
+}
